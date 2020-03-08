a52aeb9a4d0a24403b7168440d0ee623703d2423
Chris Gregg <tofergregg@gmail.com>
2020-03-08 12:55:53 -0700

    added manual time update

--- log-feed.py
+++ log-feed.py
@@ -2,6 +2,10 @@
 
 from datetime import datetime, timedelta
 import sys
+import json
+import cgi
+import cgitb
+cgitb.enable()
 
 BABY_NAME_FILE = "babyname.txt"
 
@@ -13,15 +17,47 @@ def main():
             google_voice = False
     except IndexError:
         google_voice = False
+    print("Content-Type: text/plain\n")
+    # for FieldStorage() to work,
+    # needed to change line 704 in /usr/lib/python3.7/cgi.py to:
+    # self.file.write(data.encode('utf-8')
+    try:
+        form = json.loads(sys.stdin.read())
+    except json.decoder.JSONDecodeError:
+        form = {} 
+    
+    try:
+        hour = form['hour']
+        minute = form['minute']
+        meridian = form['meridian']
+    except KeyError:
+        hour = "--" 
+        minute = "--"
+        meridian = "--"
+
+    if hour != "--":
+        hour = int(hour)
+    if minute != "--":
+        minute = int(minute)
+
+    if meridian.lower() == "pm":
+        if hour < 12:
+            hour += 12
+        if hour == 12:
+            hour = 0
 
     with open(BABY_NAME_FILE) as f:
         BABY_NAME = f.readline()[:-1]
     DATABASE = BABY_NAME.lower() + "-feedings.txt"
 
-    print("Content-Type: text/plain\n")
-
     # now = datetime.now() - timedelta(hours=1, minutes=0)
     now = datetime.now()
+   
+    if hour != "--":
+        now = now.replace(hour=hour)
+    if minute != "--":
+        now = now.replace(minute=minute)
+
     date_time = now.strftime("%m,%d,%Y,%H,%M\n")
 
     with open(DATABASE, 'a') as f:
